numbers recognize and draw done(may be need to fix vector implimentation) | paralellize signs recognition | change stream_processing to stream_utils

signs_recognize.py
@@ -1,6 +1,7 @@
 import re
 from time import time
-from typing import Iterable, Iterator
+from concurrent.futures import ThreadPoolExecutor
+from weakref import ref
 
 import cv2
 import numpy as np
@@ -8,18 +9,47 @@ import pytesseract
 from PIL import Image, ImageEnhance
 
 
-SIGN_MIN_AREA = 10000
-SIGN_MAX_AREA = 100000
+SIGN_MIN_AREA = 40000
+SIGN_MAX_AREA = 80000
 SIGN_MIN_RATIO = 4
-SIGN_MAX_RATIO = 4.5
-HSV_MIN = np.array((76, 0, 0), np.uint8)
-HSV_MAX = np.array((255, 255, 105), np.uint8)
+SIGN_MAX_RATIO = 6
+HSV_MIN = np.array((40, 0, 0), np.uint8)
+HSV_MAX = np.array((140, 255, 150), np.uint8)
 
 
-def extend_image(img: Image.Image) -> Image.Image:
+def set_hsv(path: str = 'database/test_signs_image.png'):
+	img = cv2.imread(path)
+	cv2.namedWindow('HSV')
+	cv2.createTrackbar('min_H', 'HSV', 0, 255, lambda value: value)
+	cv2.createTrackbar('min_S', 'HSV', 0, 255, lambda value: value)
+	cv2.createTrackbar('min_V', 'HSV', 0, 255, lambda value: value)
+	cv2.createTrackbar('max_H', 'HSV', 0, 255, lambda value: value)
+	cv2.createTrackbar('max_S', 'HSV', 0, 255, lambda value: value)
+	cv2.createTrackbar('max_V', 'HSV', 0, 255, lambda value: value)
+
+	while 1:
+		min_h = cv2.getTrackbarPos('min_H', 'HSV')
+		min_s = cv2.getTrackbarPos('min_S', 'HSV')
+		min_v = cv2.getTrackbarPos('min_V', 'HSV')
+		max_h = cv2.getTrackbarPos('max_H', 'HSV')
+		max_s = cv2.getTrackbarPos('max_S', 'HSV')
+		max_v = cv2.getTrackbarPos('max_V', 'HSV')
+		min_hsv = np.array([min_h, min_s, min_v], np.uint8)
+		max_hsv = np.array([max_h, max_s, max_v], np.uint8)
+
+		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		thresh = cv2.inRange(hsv, min_hsv, max_hsv)
+		cv2.imshow('2', thresh)
+		cv2.imshow('1', hsv)
+		if cv2.waitKey(5) == 27:
+			break
+	cv2.destroyAllWindows()
+
+
+def extend_image(img: Image.Image, value: float = 2.0) -> Image.Image:
 	extended = Image.new(
 		'RGB',
-		(img.width * 5, img.height * 5),
+		(round(img.width * value), round(img.height * value)),
 		(255, 255, 255)
 	)
 	extended.paste(
@@ -31,59 +61,60 @@ def extend_image(img: Image.Image) -> Image.Image:
 	return extended
 
 
-def fragmentate_sing(img: Image.Image) -> tuple[Image.Image]:
+def refragmentate_sing(img: Image.Image) -> tuple[Image.Image]:
 	img = ImageEnhance.Contrast(img).enhance(10)
 	img = ImageEnhance.Sharpness(img).enhance(5)
-	img = img.convert('L').point(lambda x: 255 if x > 100 else 0, mode='1')
+	img = img.convert('L').point(lambda x: 255 if x > 120 else 0, mode='1')
 
 	left_chars = img.crop((
-		img.width * 0.05, img.height * 0.1,
-		img.width * 0.18, img.height * 0.8
+		img.width * 0.05, img.height * 0.26,
+		img.width * 0.18, img.height * 0.88
 	))
 	right_chars = img.crop((
-		img.width * 0.5, img.height * 0.1,
-		img.width * 0.73, img.height * 0.8
+		img.width * 0.5, img.height * 0.25,
+		img.width * 0.73, img.height * 0.88
 	))
-	template = Image.new(
-		'RGB',
-		(left_chars.width + right_chars.width, left_chars.height),
-		(255, 255, 255)
-	)
-	template.paste(left_chars, (0, 0))
-	template.paste(right_chars, (left_chars.width, 0))
-	chars = extend_image(template)
-
 	numbers = img.crop((
 		img.width * 0.18, img.height * 0.1,
-		img.width * 0.5, img.height * 0.8
+		img.width * 0.5, img.height * 0.88
 	))
-	numbers = extend_image(numbers)
-
+	numbers = numbers.resize((
+		round(numbers.width * 0.75),
+		round(numbers.height * 0.75)
+	))
 	region = img.crop((
 		img.width * 0.78, img.height * 0.1,
-		img.width * 0.96, img.height * 0.55
+		img.width * 0.96, img.height * 0.58
 	))
-	region = extend_image(region)
+	refrag_sign = [left_chars, right_chars, numbers, region]
+	template = Image.new(
+		'RGB',
+		(sum(f.width for f in refrag_sign), max(f.height for f in refrag_sign)),
+		(255, 255, 255)
+	)
+	for i in range(4):
+		template.paste(
+			refrag_sign[i],
+			(sum(f.width for f in refrag_sign[:i]), 0)
+		)
+	refrag_sign = extend_image(template)
+	return refrag_sign
 
-	return chars, numbers, region
 
-
-def get_sign_str(fragments: Iterable[Image.Image]) -> str:
-	pattern = r'\W'
-	chars, numbers, region = fragments
-	chars_str = pytesseract.image_to_string(chars).upper()
-	chars_str = re.sub(pattern, '', chars_str)
-	if len(chars_str) != 3:
+def get_sign_str(refrag_sign: Image.Image) -> str:
+	sign_str = pytesseract.image_to_string(
+		refrag_sign, config='opt -fopenmp').upper()
+	sign_str = re.sub(r'\W', '', sign_str)
+	if len(sign_str) not in (8, 9):
 		return None
-	numbers_str = pytesseract.image_to_string(numbers, config='digits')
-	numbers_str = re.sub(pattern, '', numbers_str)
-	if len(numbers_str) != 3:
+	chrs = sign_str[:3]
+	if re.search('\D+', chrs) is None:
 		return None
-	region_str = pytesseract.image_to_string(region, config='digits')
-	region_str = re.sub(pattern, '', region_str)
-	if len(region_str) not in (2, 3):
+	nums = sign_str[3:6]
+	if re.search('\d+', nums) is None:
 		return None
-	sign_str = ''.join([chars_str[0], numbers_str, chars_str[1:], region_str])
+	reg = sign_str[6:]
+	sign_str = chrs[0] + nums + chrs[1:] + reg
 	return sign_str
 
 
@@ -117,38 +148,60 @@ def get_sign_rect(img: np.array, rect: tuple[tuple, tuple]) -> np.array:
 	return rotated
 
 
-def get_signs(img: np.array) -> Iterator[tuple[str, tuple]]:
-	img = cv2.medianBlur(img, 5, 0)
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	thresh = cv2.inRange(hsv, HSV_MIN, HSV_MAX)
-	contours0, _ = cv2.findContours(
-		thresh.copy(),
-		cv2.RETR_TREE,
-		cv2.CHAIN_APPROX_SIMPLE
-	)
-
-	for cnt in contours0:
-		rect = cv2.minAreaRect(cnt)
+def get_relative_rects(contours: list[np.array]) -> list[tuple]:
+	rects = []
+	for contour in contours:
+		rect = cv2.minAreaRect(contour)
 		area = int(rect[1][0] * rect[1][1])
 		if not (SIGN_MIN_AREA < area < SIGN_MAX_AREA):
 			continue
-
 		ratio = rect[1][1] / rect[1][0]
 		ratio = max(ratio, 1 / ratio)
 		if not (SIGN_MIN_RATIO < ratio < SIGN_MAX_RATIO):
 			continue
+		rects += [rect]
+	return rects
 
-		sign = get_sign_rect(img, rect)
-		pil_img = Image.fromarray(cv2.cvtColor(sign, cv2.COLOR_BGR2RGB))
-		fragments = fragmentate_sing(pil_img)
-		sign_str = get_sign_str(fragments)
-		if sign_str:
-			yield (sign_str, rect)
+
+def get_sign(img: np.array, rect: tuple[tuple, tuple]) -> 'str|None':
+	sign = get_sign_rect(img, rect)
+	pil_img = Image.fromarray(cv2.cvtColor(sign, cv2.COLOR_BGR2RGB))
+	refrag_sign = refragmentate_sing(pil_img)
+	sign_str = get_sign_str(refrag_sign)
+	if sign_str:
+		return sign_str, rect
+	return None
+
+
+def signs_from_frame(img: np.array) -> list[tuple[str, tuple]]:
+	img = cv2.medianBlur(img, 5, 0)
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	thresh = cv2.inRange(hsv, HSV_MIN, HSV_MAX)
+	contours, _ = cv2.findContours(
+		thresh.copy(),
+		cv2.RETR_TREE,
+		cv2.CHAIN_APPROX_SIMPLE
+	)
+	rects = get_relative_rects(contours)
+	workers = len(rects)
+	if not workers:
+		return []
+	with ThreadPoolExecutor(workers) as executor:
+		signs = executor.map(
+			get_sign,
+			[img for _ in range(workers)],
+			rects
+		)
+	return [sign for sign in signs if sign is not None]
+
+
+def signs_from_file(file_path: str) -> list[tuple[str, tuple]]:
+	img = cv2.imread(file_path)
+	return signs_from_frame(img)
 
 
 if __name__ == '__main__':
-	img = cv2.imread('database/test_signs_image.png')
 	st = time()
-	signs = get_signs(img).__next__()
-	print(time() - st)
+	signs = signs_from_file('database/test_signs_image.png')
+	print('Full time:', time() - st)
 	print(signs)

widgets/streamview.py
@@ -1,36 +1,66 @@
 import io
 
+import numpy as np
+from database.db import get_bad_cars, get_bad_people
 from kivy.clock import Clock
 from kivy.core.image import Image as CoreImage
 from kivy.uix.image import Image
-from kivy.uix.widget import Widget
+from pioneer_sdk import Pioneer
+from signs_recognize import signs_from_frame
+from stream_utils import draw_sign, frame_to_img, img_to_buf
 
 
-class StreamView(Image):	
+class StreamView(Image):
 	size_hint = 1, 1
 
-	def __init__(self, drone, **kwargs):
+	def __init__(self, drone: Pioneer, mode: str, **kwargs):
 		super().__init__(**kwargs)
+		self._mode = mode
+		self._frame_counter = 0
+		self._vec_k = 0.1
+		
 		self.drone = drone
-		self.update_trigger = Clock.create_trigger(
+		self.offences = get_bad_cars()
+		self.signs = []
+
+		self.update_event = Clock.schedule_interval(
 			self._update_frame,
-			timeout=0.05,
-			interval=True
+			timeout=0.05
 		)
+		self.update_event()
 
 	def on_parent(self, *args):
-		if self.parent is not None:
-			self.update_trigger()
-		else:
-			self.update_trigger.close()
+		if self.parent is None:
+			self.update_event.cancel()
 
-	def _bytes_to_img(self, image_bytes: bytes):
+	def _frame_process_signs(self, img: np.array) -> bytes:
+		if self._frame_counter == 15:
+			self._frame_counter = 0
+			self.signs = signs_from_frame(img)
+
+		for sign in self.signs:
+			offence = self.offences.get(sign[0], None)
+			if offence is not None:
+				img = draw_sign(
+					img, 
+					offence, 
+					sign[1], 
+					(0, self._frame_counter * self._vec_k)
+				)
+		self._frame_counter += 1
+		return img_to_buf(img)
+
+	def _bytes_to_frame(self, image_bytes: bytes) -> CoreImage:
 		buf = io.BytesIO(image_bytes)
 		cim = CoreImage(buf, ext='png')
-		return Image(texture=cim.texture)
+		return cim
 
 	def _update_frame(self, *args):
-		print('Update frame')
 		camera_frame = self.drone.get_raw_video_frame()
-		image = self._bytes_to_img(camera_frame)
-		self.texture = image.texture
+		img = frame_to_img(camera_frame)
+
+		if self._mode == 'signs':
+			img_buf = self._frame_process_signs(img)
+
+		frame = self._bytes_to_frame(img_buf)
+		self.texture = frame.texture